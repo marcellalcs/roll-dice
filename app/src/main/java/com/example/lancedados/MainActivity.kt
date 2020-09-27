5a7ec1fd801a414a191558573f6788d2eaf37fa2
Marcella Souza <mlsouza@thoughtworks.com>
2020-09-27 18:44:36 -0300

    feat: 🎸 Showing two random numbers

--- app/src/main/java/com/example/lancedados/MainActivity.kt
+++ app/src/main/java/com/example/lancedados/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.lancedados
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoJogar = findViewById<Button>(R.id.botaoJogar)
+        val textoDado1 = findViewById<TextView>(R.id.dado1)
+        val textoDado2 = findViewById<TextView>(R.id.dado2)
+
+        botaoJogar.setOnClickListener {
+            textoDado1.text = gerarNumero().toString()
+            textoDado2.text = gerarNumero().toString()
+
+            textoDado1.setTextColor(Color.parseColor("#FF0000"))
+            textoDado2.setTextColor(Color.parseColor("#FF0000"))
+        }
     }
+
+    fun gerarNumero(): Int {
+        return (1..6).random()
+    }
+
 }
