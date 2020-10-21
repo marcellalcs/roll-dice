f50051ca1b2058c143ffaf14e96fdcfaa4f0ad15
Marcella Souza <mlsouza@thoughtworks.com>
2020-10-21 20:30:51 -0300

    primeiro mvp  lanca dados

---
+++ app/src/main/java/com/wcc/lancedados/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.wcc.lancedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoLancarDados = findViewById<Button>(R.id.lanceDadosBotao)
+        val valorDado1 = findViewById<TextView>(R.id.dado1)
+
+        botaoLancarDados.setOnClickListener {
+            valorDado1.text = lancarDados().toString()
+        }
+
+    }
+
+    fun lancarDados(): Int {
+        return (1..6).random()
+    }
+
+}
